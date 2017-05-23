af4801f77c03b21bf87830accc56bdbaa9f803f7
Peter Lawless <peterlawless223@gmail.com>
2017-05-23 06:15:57 -0400

    Added commentary for devise customization

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,4 +1,10 @@
 class User::RegistrationsController < Devise::RegistrationsController
+  # https://github.com/plataformatec/devise#strong-parameters
+  # This allows you to add new attributes to forms than
+  # what comes default with devise.
+  # It inherits from Devise::RegistrationsController so that whatever
+  # methods not defined here will be picked up in the default registrations
+  # controller.
   before_filter :configure_permitted_parameters
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # this line allows us to use a custom controller for registrations
   devise_for :users, :controllers => { :registrations => 'user/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
