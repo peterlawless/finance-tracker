62d2a7a8c2532610304a8d487852ecce02a4a697
Peter Lawless <peterlawless223@gmail.com>
2017-05-23 07:37:00 -0400

    Created listing for friendships in my_friends view

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
   get 'my_friends', to: 'users#my_friends'
 end
